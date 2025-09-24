43b7d0cfd50fa9e141db1ab1aea91d06cc74e7ab
Kavita <kavitaninave123@gmail.com>
2025-09-24 18:59:54 +0530

    Updated in orderDetails

--- screens/Account/OrderDetail.js
+++ screens/Account/OrderDetail.js
@@ -99,7 +99,7 @@ const OrderDetail = ({ navigation, route }) => {
       <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
         <View style={styles.headerContainer}>
           <LinearGradient
-            colors={["#1e3c72", "#2a5298"]}
+            colors={["#0075f8", "#0075f8"]}
             style={styles.headerGradient}
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 0 }}
